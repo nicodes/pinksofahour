aa8d55ab5413a8d4098ab70cd4dae71f4daf1c5b
nicodes <nicozessoules@gmail.com>
2023-04-01 14:47:45 -0600

    setup basic episodes grid view

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -1,9 +1,62 @@
-interface JsonFile {
-  [key: string]: any;
-}
-
-export async function readJsonFileFromDrive(fileId: string): Promise<JsonFile> {
+/** Gets public json from gdrive file id */
+export async function readJsonFileFromDrive(fileId: string) {
   const fileUrl = `https://drive.google.com/uc?id=${fileId}&export=download`;
   const fileResponse = await fetch(fileUrl);
   return fileResponse.json();
+}
+
+/** Gets most recent N videos from Pink Sofa Hour youtube channel */
+export async function getRecentFromChannel(maxResults: number = 5) {
+  const requestUrl =
+    "https://www.googleapis.com/youtube/v3/search?" +
+    `key=${import.meta.env.GOOGLE_API_KEY}` +
+    `&channelId=${import.meta.env.YOUTUBE_CHANNEL_ID}` +
+    "&part=snippet,id" +
+    "&order=date" +
+    `&maxResults=${maxResults}`;
+
+  const response = await fetch(requestUrl);
+  const data = await response.json();
+
+  const videos = [...data.items].map((item: any) => {
+    return {
+      id: item.id.videoId,
+      title: item.snippet.title,
+      thumbnail: item.snippet.thumbnails.default.url,
+    };
+  });
+
+  return videos;
+}
+
+/** Gets most recent N videos from Pink Sofa Hour youtube playlist */
+export async function getVideosFromPlaylist(
+  playlist: "episodes" | "performances",
+  maxResults: number = 5
+) {
+  const playlistId =
+    playlist === "episodes"
+      ? import.meta.env.YOUTUBE_EPISODES_PLAYLIST_ID
+      : import.meta.env.YOUTUBE_PERFORMANCES_PLAYLIST_ID;
+
+  const requestUrl =
+    "https://www.googleapis.com/youtube/v3/playlistItems?" +
+    `key=${import.meta.env.GOOGLE_API_KEY}` +
+    `&playlistId=${playlistId}` +
+    "&part=snippet,id" +
+    "&order=date" +
+    `&maxResults=${maxResults}`;
+
+  const response = await fetch(requestUrl);
+  const data = await response.json();
+
+  const videos = [...data.items].map((item: any) => {
+    return {
+      id: item.snippet.resourceId.videoId,
+      title: item.snippet.title,
+      thumbnail: item.snippet.thumbnails.default.url,
+    };
+  });
+
+  return videos;
 }
