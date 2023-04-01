f1060ad0e920e1c0ed5220e97a7df4b5bf217e77
nicodes <nicozessoules@gmail.com>
2023-04-01 12:21:13 -0600

    add preact and update .env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
+  integrations: [preact()]
 });
