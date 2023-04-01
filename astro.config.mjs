8c20c676fc78b6e292ff0f9a2073ade56aeae5e5
nicodes <nicozessoules@gmail.com>
2023-04-01 14:52:01 -0600

    remove preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [preact()]
 });
