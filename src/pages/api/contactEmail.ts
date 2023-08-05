6a15a6ea661ce572f6700bba291de5b14d38c70a
nicodes <nicozessoules@gmail.com>
2023-08-05 04:18:55 -0600

    fix api error

--- src/pages/api/contactEmail.ts
+++ src/pages/api/contactEmail.ts
@@ -12,50 +12,76 @@ apiInstance.setApiKey(
 const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
 
 export const post: APIRoute = async function get({ request, redirect }) {
-  const formData = await request.formData();
+  try {
+    const formData = await request.formData();
+    const name = formData.get("name");
 
-  sendSmtpEmail.sender = {
-    name: "Pink Sofa Hour",
-    email: "pinksofahour@gmail.com",
-  };
-  sendSmtpEmail.to = [
-    { email: "nicozessoules@gmail.com", name: "Pink Sofa Hour" },
-  ];
-  sendSmtpEmail.subject = "PSH Contact: " + formData.get("name");
-  // sendSmtpEmail.htmlContent = "<html><body><p>test</p></body></html>";
-  sendSmtpEmail.htmlContent = `<html><body>
-  <p>New PSH Contact</p>
-  <ul>
-  <li><strong>Name:</strong> ${formData.get("name")}</li>
-  <li><strong>Previous Interviews:</strong> ${formData.get(
-    "previous-interviews"
-  )}</li>
-  <li><strong>Additional Links:</strong> ${formData.get(
-    "additional-links"
-  )}</li>
-  <li><strong>How did you hear about PSH:</strong> ${formData.get(
-    "how-did-you-hear"
-  )}</li>
-  <li><strong>Favorite thing about your local music scene:</strong> ${formData.get(
-    "favorite-thing"
-  )}</li>
-  </ul>
-  </body></html>`;
+    if (!name) {
+      return new Response(
+        JSON.stringify({
+          message: "Missing required fields",
+        }),
+        { status: 400 }
+      );
+    }
 
-  let hasError = false;
-  const res = await apiInstance.sendTransacEmail(sendSmtpEmail);
+    sendSmtpEmail.sender = {
+      name: "Pink Sofa Hour",
+      email: "pinksofahour@gmail.com",
+    };
+    sendSmtpEmail.to = [
+      { email: "nicozessoules@gmail.com", name: "Pink Sofa Hour" },
+    ];
 
-  // .then(
-  //   function (data) {
-  //     console.log(
-  //       "API called successfully. Returned data: " + JSON.stringify(data)
-  //     );
-  //   },
-  //   function (error) {
-  //     hasError = true;
-  //     console.error(error);
-  //   }
-  // );
+    // sendSmtpEmail.subject = "PSH Contact: ";
+    sendSmtpEmail.subject = "PSH Contact: " + name;
 
-  redirect("/contact", 307);
+    sendSmtpEmail.htmlContent = "<html><body><p>test</p></body></html>";
+    // sendSmtpEmail.htmlContent = `<html><body>
+    // <p>New PSH Contact</p>
+    // <ul>
+    // <li><strong>Name:</strong> ${formData.get("name")}</li>
+    // <li><strong>Previous Interviews:</strong> ${formData.get(
+    //   "previous-interviews"
+    // )}</li>
+    // <li><strong>Additional Links:</strong> ${formData.get(
+    //   "additional-links"
+    // )}</li>
+    // <li><strong>How did you hear about PSH:</strong> ${formData.get(
+    //   "how-did-you-hear"
+    // )}</li>
+    // <li><strong>Favorite thing about your local music scene:</strong> ${formData.get(
+    //   "favorite-thing"
+    // )}</li>
+    // </ul>
+    // </body></html>`;
+
+    let hasError = false;
+    const res = await apiInstance.sendTransacEmail(sendSmtpEmail);
+
+    // .then(
+    //   function (data) {
+    //     console.log(
+    //       "API called successfully. Returned data: " + JSON.stringify(data)
+    //     );
+    //   },
+    //   function (error) {
+    //     hasError = true;
+    //     console.error(error);
+    //   }
+    // );
+
+    return {
+      body: JSON.stringify({
+        name: "all good",
+      }),
+    };
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        message: "Missing required fields 404",
+      }),
+      { status: 400 }
+    );
+  }
 };
