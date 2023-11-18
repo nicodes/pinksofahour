b39e8bfa964f129131a953c150373854ee3022ee
Nico Zessoules <35477151+nicodes@users.noreply.github.com>
2023-11-18 11:39:10 -0700

    Add Contact Email Page (#23)
    * add brevo support
    
    * cleanup
    
    * disable resize for textarea
    
    * update env vars
    
    * fix label spacing
    
    * add svelte
    
    * add hamburger and restyle header
    
    * fix formatting
    
    * add cursor pointer
    
    * add pure css hashing to prod
    
    * add htmx form response

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from "astro/config";
-
+import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
+  integrations: [svelte()],
+  vite: {
+    css: {
+      modules: {
+        hash:
+          import.meta.env.VERCEL_ENV === "production" ? "base64:5" : undefined,
+      },
+    },
+  },
   experimental: {
     assets: true,
     viewTransitions: true,
---
+++ src/components/hamburger/Hamburger.svelte
@@ -0,0 +1,25 @@
+<script>
+    import styles from './hamburger.module.scss'
+
+    let open = false;
+  
+    function toggleMenu() {
+      open = !open;
+    }
+</script>
+
+<div class={styles.container}>
+    <button on:click={toggleMenu} class={styles.button}>
+        <img src={'/hamburger.svg'} alt="hamburger" />
+    </button>
+    {#if open}
+        <div class={styles.clickCatcher} on:click={toggleMenu} />
+        <div class={styles.menu}>
+            <a href="/" on:click={toggleMenu}>Home</a>
+            <a href="events" on:click={toggleMenu}>Events</a>
+            <a href="contact" on:click={toggleMenu}>Contact</a>
+            <a href="https://www.youtube.com/@pinksofahour">Youtube</a>
+            <a href="https://www.instagram.com/pinksofahour">Instagram</a>
+        </div>        
+    {/if}
+</div>
