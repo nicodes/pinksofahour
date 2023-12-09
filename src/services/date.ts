82baedab84d3483a7eb6da1108018d280df5552c
Nico Zessoules <nicozessoules@gmail.com>
2023-12-09 02:25:56 -0700

    try with locale time

--- src/services/date.ts
+++ src/services/date.ts
@@ -22,22 +22,14 @@ const months = [
   "November",
   "December",
 ];
-/** Format date into 12 hour format */
-export function formatTime(date: Date): string {
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const ampm = hours >= 12 ? "PM" : "AM";
-
-  const formattedHours = hours % 12 || 12; // the hour '0' should be '12'
-  const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
-
-  return `${formattedHours}:${formattedMinutes} ${ampm}`;
-}
 
 /** Parse string date into { date, day, month, time? } */
-export const getDateLabel = (dateInput: Date) => ({
-  day: days[dateInput.getDay()],
-  date: dateInput.getDate().toString(),
-  month: months[dateInput.getMonth()],
-  time: formatTime(dateInput),
+export const getDateLabel = (date: Date) => ({
+  day: days[date.getDay()],
+  date: date.getDate().toString(),
+  month: months[date.getMonth()],
+  time: date.toLocaleTimeString([], {
+    hour: "numeric",
+    minute: "2-digit",
+  }),
 });
